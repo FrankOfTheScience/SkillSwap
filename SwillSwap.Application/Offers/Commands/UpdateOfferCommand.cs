53e832104c00331f55717a5c044405b65cb0f860
FrankOfTheScience <119010269+FrankOfTheScience@users.noreply.github.com>
2025-09-23 12:02:25 +0200

    feat: Implemented GET by Id, PUT and DELETE methods for the /offers API with Validators

---
+++ SwillSwap.Application/Offers/Commands/UpdateOfferCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using SkillSwap.Application.Offers.Dtos;
+
+namespace SkillSwap.Application.Offers.Commands;
+public record UpdateOfferCommand(int Id, string Title, string Description, decimal Price, int CreatedBy) : IRequest<OfferDto?>;
