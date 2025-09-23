f2c61d0e9e7f9d3229baaf5b8cfa7e851d7de18b
FrankOfTheScience <119010269+FrankOfTheScience@users.noreply.github.com>
2025-09-23 13:38:22 +0200

    tests: Implemented unit tests and modified CI pipeline

---
+++ SkillSwap.Tests/Validators/CreateOfferCommandValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using SkillSwap.Application.Offers.Commands;
+using SkillSwap.Application.Offers.Validators;
+
+namespace SkillSwap.Tests.Validators;
+public class CreateOfferCommandValidatorTests
+{
+    private readonly CreateOfferCommandValidator _validator = new CreateOfferCommandValidator();
+
+    [Fact]
+    public void Should_Have_Error_When_Title_Is_Empty()
+    {
+        var cmd = new CreateOfferCommand("", "d", 0m, 0);
+        var result = _validator.TestValidate(cmd);
+        result.ShouldHaveValidationErrorFor(c => c.Title);
+        result.ShouldHaveValidationErrorFor(c => c.Price);
+        result.ShouldHaveValidationErrorFor(c => c.CreatedBy);
+    }
+
+    [Fact]
+    public void Should_Pass_When_Valid()
+    {
+        var cmd = new CreateOfferCommand("t", "d", 10m, 1);
+        var result = _validator.TestValidate(cmd);
+        result.IsValid.Should().BeTrue();
+    }
+}
